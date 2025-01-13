9fba180535f3b923d62138bba2efcf9edb12a177
ElCald <eliot.cal@gmail.com>
2025-01-13 12:00:24 +0100

    script sh Lv7

--- Langfordv07/send_version_opti/mpi_exe.sh
+++ Langfordv07/send_version_opti/mpi_exe.sh
@@ -7,6 +7,7 @@ if [ -n $1 ]; then
     make
     echo "Exécution avec MPI"
     mpirun --use-hwthread-cpus --allow-run-as-root --oversubscribe -n $1 ./main >> result.txt
+    echo "Résultat sauvegardé dans result.txt"
     echo "Clean des fichiers"
     make clean
 
